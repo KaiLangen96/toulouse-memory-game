479b656072fa1eade8a4fc9942362b5a82586528
KaiLangen96 <>
2023-05-19 11:15:35 +0000

    Add matching cards functions in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,3 +33,47 @@ function flipCard() {
     console.log('Two card has been flipped');
     checkMatch();
 }
+
+//check if cards match:
+function checkMatch() {
+    console.log('A check has been performed');
+    let match = firstCard.dataset.card === secondCard.dataset.card;
+    match ? matchedCards() : resetCards();
+}
+
+//if matched:
+function matchedCards() {
+    console.log('It is a match');
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    continuePlaying();
+    //add one to moves:
+    moves++;
+    counterMoves.textContent = moves;
+    //add one to score:
+    points++;
+    counterPoints.textContent = points;
+    endGame();
+}
+
+//if not a match:
+function resetCards() {
+    console.log('It is not a match');
+    noFlip = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        continuePlaying();
+    }, 700);
+    moves++;
+    counterMoves.textContent = moves;
+}
+
+//contine the game:
+function continuePlaying() {
+    console.log('The game continues');
+    hasFlipped = false;
+    noFlip = false;
+    firstCard = null;
+    secondCard = null;
+}
